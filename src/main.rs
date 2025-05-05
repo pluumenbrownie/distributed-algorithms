05266e113cf7d07ceb4003ede88fdfaf42625534
pluumenbrownie <git.wessel@beumertjes.nl>
2025-05-05 17:41:55 +0200

    Working load and save dialogs

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use anyhow::{Context, Result};
 use ratatui::{
     DefaultTerminal, Frame,
     buffer::Buffer,
-    crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind},
+    crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind, KeyModifiers},
     layout::{Constraint, Flex, Layout, Rect},
     style::{Style, Stylize},
     symbols::border,
@@ -12,8 +12,9 @@ use ratatui::{
 };
 use serde::{Deserialize, Serialize};
 use std::{
-    fs,
+    env, fs,
     io::{self, Write},
+    path::{Path, PathBuf},
 };
 use tui_textarea::TextArea;
 
@@ -27,6 +28,73 @@ const NODE_HEIGHT: u16 = 3;
 const NODE_WIDTH: u16 = 6;
 const NODE_H_SPACING: u16 = 4;
 const NODE_V_SPACING: u16 = 2;
+
+#[derive(Debug, Default, Clone, Copy, PartialEq, Eq)]
+enum PopupState {
+    #[default]
+    Off,
+    Save,
+    Load,
+    Small,
+    Large,
+}
+
+#[derive(Debug, Default, Clone, Copy)]
+enum PopupSize {
+    #[default]
+    Small = 3,
+    Large = 20,
+}
+
+impl PopupState {
+    fn size(self) -> PopupSize {
+        match self {
+            Self::Save => PopupSize::Small,
+            Self::Load => PopupSize::Small,
+            Self::Small => PopupSize::Small,
+            Self::Large => PopupSize::Large,
+            _ => panic!("Got property of illegal state."),
+        }
+    }
+
+    fn title_top<'a>(self) -> Line<'a> {
+        match self {
+            Self::Save => Line::from(" Save structure to... ").left_aligned(),
+            Self::Load => Line::from(" Load structure ... ").left_aligned(),
+            Self::Small => Line::from(" Small Popup ").left_aligned(),
+            Self::Large => Line::from(" Large Popup ").left_aligned(),
+            _ => panic!("Got property of illegal state."),
+        }
+    }
+
+    fn title_bottom<'a>(self) -> Line<'a> {
+        match self {
+            Self::Save => Line::from(" <Esc> Cancel - <Enter> Save ").right_aligned(),
+            Self::Load => Line::from(" <Esc> Cancel - <Enter> Load ").right_aligned(),
+            Self::Small => Line::from(" Close with <Esc> ").right_aligned(),
+            Self::Large => Line::from(" Close with <Esc> ").right_aligned(),
+            _ => panic!("Got property of illegal state."),
+        }
+    }
+
+    fn content_default(self, latest_dir: &PathBuf, latest_file: &String) -> String {
+        match self {
+            Self::Save => {
+                let mut full_file = latest_dir.clone();
+                full_file.push(latest_file);
+                full_file.display().to_string()
+            }
+            Self::Load => {
+                let mut full_file = latest_dir.clone();
+                full_file.push(latest_file);
+                full_file.display().to_string()
+            }
+            Self::Small => String::from(""),
+            Self::Large => String::from(""),
+            _ => panic!("Got property of illegal state."),
+        }
+    }
+}
 
 #[derive(Debug, Default, Clone, Serialize, Deserialize)]
 pub struct NodeGrid {
@@ -43,9 +111,11 @@ pub struct NodeGridDisplay<'a> {
 pub struct App<'a> {
     exit: bool,
     node_display: NodeGridDisplay<'a>,
-    show_grid: bool,
-    show_popup: bool,
+    // show_grid: bool,
+    show_popup: PopupState,
     textarea: TextArea<'a>,
+    latest_dir: PathBuf,
+    latest_file: String,
 }
 
 fn main() -> Result<()> {
@@ -54,6 +124,8 @@ fn main() -> Result<()> {
     let node_display = NodeGridDisplay::new(grid.clone());
     let mut app = App {
         node_display,
+        latest_dir: env::current_dir()?,
+        latest_file: String::from("grid.json"),
         ..Default::default()
     };
     let app_result = app.run(&mut terminal);
@@ -74,22 +146,23 @@ impl App<'_> {
 
     fn draw(&self, frame: &mut Frame) {
         frame.render_widget(self, frame.area());
-
-        if self.show_popup {
-            frame.render_widget(&self.textarea, popup_area(frame.area(), 60, 20));
-        }
     }
 
     fn handle_events(&mut self) -> Result<()> {
-        if self.show_popup {
-            self.handle_textarea()?;
-        } else {
-            match event::read()? {
-                Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
-                    self.handle_key_event(key_event)?
-                }
-                _ => {}
-            };
+        match self.show_popup {
+            PopupState::Off => {
+                match event::read()? {
+                    Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
+                        self.handle_key_event(key_event)?
+                    }
+                    _ => {}
+                };
+            }
+            PopupState::Save => self.save_textarea()?,
+            PopupState::Load => self.load_textarea()?,
+            _ => {
+                self.handle_textarea()?;
+            }
         }
         Ok(())
     }
@@ -98,7 +171,56 @@ impl App<'_> {
         match event::read()? {
             Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
                 match key_event.code {
-                    KeyCode::Esc => self.popup(),
+                    KeyCode::Esc => self.close_popup(),
+                    _ => {
+                        self.textarea.input(key_event);
+                    }
+                }
+            }
+            _ => {}
+        };
+        Ok(())
+    }
+
+    fn save_textarea(&mut self) -> Result<()> {
+        match event::read()? {
+            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
+                match key_event.code {
+                    KeyCode::Esc => self.close_popup(),
+                    KeyCode::Enter => {
+                        let mut path: PathBuf = self.textarea.lines()[0].parse()?;
+                        self.save_grid(&path)?;
+                        self.latest_file = path.pop().to_string();
+                        self.latest_dir = path;
+                        self.close_popup();
+                    }
+                    _ => {
+                        self.textarea.input(key_event);
+                    }
+                }
+            }
+            _ => {}
+        };
+        Ok(())
+    }
+
+    fn load_textarea(&mut self) -> Result<()> {
+        match event::read()? {
+            Event::Key(key_event) if key_event.kind == KeyEventKind::Press => {
+                match key_event.code {
+                    KeyCode::Esc => self.close_popup(),
+                    KeyCode::Enter => {
+                        let mut path: PathBuf = self.textarea.lines()[0].parse()?;
+                        self.load_grid(&path)?;
+                        self.latest_file = path
+                            .file_name()
+                            .expect("I mean I didn't WANT to unwrap here...")
+                            .to_string_lossy()
+                            .to_string();
+                        path.pop();
+                        self.latest_dir = path;
+                        self.close_popup();
+                    }
                     _ => {
                         self.textarea.input(key_event);
                     }
@@ -112,31 +234,33 @@ impl App<'_> {
     fn handle_key_event(&mut self, key_event: KeyEvent) -> Result<()> {
         match key_event.code {
             KeyCode::Char('q') => self.exit(),
-            KeyCode::Char('s') => self.save_grid()?,
-            KeyCode::Char('l') => self.load_grid()?,
-            KeyCode::Char('t') => self.popup(),
+            KeyCode::Char('s') if key_event.modifiers.contains(KeyModifiers::CONTROL) => {
+                self.open_popup(PopupState::Save);
+            }
+            KeyCode::Char('o') if key_event.modifiers.contains(KeyModifiers::CONTROL) => {
+                self.open_popup(PopupState::Load);
+            }
+            KeyCode::Char('t') => self.open_popup(PopupState::Small),
+            KeyCode::Char('y') => self.open_popup(PopupState::Large),
             _ => {}
         }
         Ok(())
     }
 
-    fn load_grid(&mut self) -> Result<()> {
-        let file = fs::OpenOptions::new().read(true).open("grids/grid.json")?;
+    fn load_grid(&mut self, path: &PathBuf) -> Result<()> {
+        let file = fs::OpenOptions::new().read(true).open(path)?;
         let reader = io::BufReader::new(file);
         self.node_display.grid = serde_json::from_reader(reader)?;
 
         Ok(())
     }
 
-    fn save_grid(&self) -> Result<()> {
-        if !fs::exists("grids")? {
-            fs::create_dir("grids")?;
-        };
+    fn save_grid(&self, path: &PathBuf) -> Result<()> {
         let file = fs::OpenOptions::new()
             .create(true)
             .truncate(true)
             .write(true)
-            .open("grids/grid.json")?;
+            .open(path)?;
         let mut writer = io::BufWriter::new(file);
         serde_json::to_writer_pretty(&mut writer, &self.node_display.grid)?;
         writer.flush()?;
@@ -147,11 +271,27 @@ impl App<'_> {
         self.exit = true;
     }
 
-    // fn type_something(&self) {}
-    fn popup(&mut self) {
-        self.textarea.select_all();
-        self.textarea.delete_char();
-        self.show_popup = !self.show_popup;
+    fn open_popup(&mut self, state: PopupState) {
+        if state == PopupState::Off {
+            panic!("")
+        }
+        self.textarea = TextArea::new(vec![
+            state.content_default(&self.latest_dir, &self.latest_file),
+        ]);
+        self.textarea.set_block(
+            Block::bordered()
+                .title_top(state.title_top())
+                .title_bottom(state.title_bottom()),
+        );
+
+        // Move cursor to the end of the text
+        self.textarea
+            .input(KeyEvent::new(KeyCode::End, KeyModifiers::CONTROL));
+        self.show_popup = state;
+    }
+
+    fn close_popup(&mut self) {
+        self.show_popup = PopupState::Off;
     }
 }
 
@@ -176,13 +316,30 @@ impl Widget for &App<'_> {
 
         self.node_display.clone().block(block).render(area, buf);
 
-        if self.show_popup {
-            let block = Block::bordered().title("Popup");
-            let area = popup_area(area, 60, 20);
-            Clear.render(area, buf);
-            block.render(area, buf);
+        match self.show_popup {
+            PopupState::Off => (),
+            size => match size.size() {
+                PopupSize::Small => {
+                    let area = popup_area_small(area, 60, 3);
+                    Clear.render(area, buf);
+                    self.textarea.render(area, buf);
+                }
+                PopupSize::Large => {
+                    let area = popup_area(area, 60, 20);
+                    Clear.render(area, buf);
+                    self.textarea.render(area, buf);
+                }
+            },
         }
     }
+}
+
+fn popup_area_small(area: Rect, percent_x: u16, length_y: u16) -> Rect {
+    let vertical = Layout::vertical([Constraint::Length(length_y)]).flex(Flex::Center);
+    let horizontal = Layout::horizontal([Constraint::Percentage(percent_x)]).flex(Flex::Center);
+    let [area] = vertical.areas(area);
+    let [area] = horizontal.areas(area);
+    area
 }
 
 fn popup_area(area: Rect, percent_x: u16, percent_y: u16) -> Rect {
